2aba3987f356b445f78e36eaa0960d8a61e31606
ezrasujeevan <ezra.sujeevan@gmail.com>
2024-10-28 13:43:27 +0530

    Refactor app.controller.ts and app.service.ts
    - Add new API endpoints for adding, removing, and retrieving products from the cart
    - Implement functionality to add, remove, and retrieve products from the cart in app.service.ts
    - Update app.controller.ts to include the new API endpoints
    - Update app.service.ts to include interfaces for Product and Cart
    - Update app.service.ts to use the new interfaces for inventories and cart
    - Update main.ts to enable CORS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService, Cart, Product } from './app.service';
 
 @Controller()
 export class AppController {
@@ -8,5 +8,30 @@ export class AppController {
   @Get()
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Post('/add')
+  addToCart(@Body() id: number): Cart[] {
+    return this.appService.addToCart(id);
+  }
+
+  @Post('/remove')
+  removeFromCart(@Body() id: number): Cart[] {
+    return this.appService.removeFromCart(id);
+  }
+
+  @Get('/cart')
+  showAllProductsInCart(): Cart[] {
+    return this.appService.showAllProductsInCart();
+  }
+
+  @Get('/products')
+  showAllProducts(): Product[] {
+    return this.appService.getAllProducts();
+  }
+
+  @Get('/total')
+  getTotal(): number {
+    return this.appService.getTotal();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,70 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Product {
+  id: number;
+  name: string;
+  unitPrice: number;
+}
+export interface Cart extends Product {
+  quantity: number;
+}
+
 @Injectable()
 export class AppService {
-  private inventories = [
-    { name: 'bacon', unitPrice: 10.99, quantity: 10 },
-    { name: 'eggs', unitPrice: 3.99, quantity: 10 },
-    { name: 'cheese', unitPrice: 6.99, quantity: 10 },
-    { name: 'chives', unitPrice: 1.0, quantity: 10 },
-    { name: 'wine', unitPrice: 11.99, quantity: 10 },
-    { name: 'brandy', unitPrice: 17.55, quantity: 10 },
-    { name: 'bananas', unitPrice: 0.69, quantity: 10 },
-    { name: 'ham', unitPrice: 2.69, quantity: 10 },
-    { name: 'tomatoes', unitPrice: 3.26, quantity: 10 },
-    { name: 'tissue', unitPrice: 8.45, quantity: 10 },
+  private inventories: Product[] = [
+    { id: 1, name: 'bacon', unitPrice: 10.99 },
+    { id: 2, name: 'eggs', unitPrice: 3.99 },
+    { id: 3, name: 'cheese', unitPrice: 6.99 },
+    { id: 4, name: 'chives', unitPrice: 1.0 },
+    { id: 5, name: 'wine', unitPrice: 11.99 },
+    { id: 6, name: 'brandy', unitPrice: 17.55 },
+    { id: 7, name: 'bananas', unitPrice: 0.69 },
+    { id: 8, name: 'ham', unitPrice: 2.69 },
+    { id: 9, name: 'tomatoes', unitPrice: 3.26 },
+    { id: 10, name: 'tissue', unitPrice: 8.45 },
   ];
+
+  private cart: Cart[] = [];
+
   getHello() {
+    return 'Hello World!';
+  }
+
+  addToCart(id: number) {
+    const product = this.inventories.find((product) => product.id === id);
+    if (product) {
+      const cartItem = this.cart.find((cartItem) => cartItem.id === id);
+      if (cartItem) {
+        cartItem.quantity++;
+      } else {
+        this.cart.push({ ...product, quantity: 1 });
+      }
+    }
+    return this.cart;
+  }
+
+  removeFromCart(id: number) {
+    const cartItem = this.cart.find((cartItem) => cartItem.id === id);
+    if (cartItem) {
+      cartItem.quantity--;
+      if (cartItem.quantity === 0) {
+        this.cart = this.cart.filter((cartItem) => cartItem.id !== id);
+      }
+    }
+    return this.cart;
+  }
+
+  showAllProductsInCart() {
+    return this.cart;
+  }
+
+  getAllProducts() {
     return this.inventories;
+  }
+
+  getTotal() {
+    return this.cart.reduce((acc, cartItem) => {
+      return acc + cartItem.unitPrice * cartItem.quantity;
+    }, 0);
   }
 }
